b14d518c522a86d3a5a415f740c261574f8828b2
roberto <63375372+xROBERTINOx@users.noreply.github.com>
2022-12-02 01:41:52 +0000

    added 24h change but need to make format look better

--- main.py
+++ main.py
@@ -2,30 +2,35 @@
 import requests
 # python -m pip install beautifulsoup4
 from bs4 import BeautifulSoup
+
 URL = "https://public.com/crypto/"
 page = requests.get(URL)
-soup= BeautifulSoup(page.content, "html.parser")
+soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find(id="page-wrap")
 cryptoStats = results.find_all("div", class_="content-wrap")
 
 dictOfTickerToLastPrices = {}
 dictOfNameToTicker = {}
-  
-# add 24 hour change 
+
+# add 24 hour change F(X) 
+# add check for price change of other times (week)
 for cryptoStat in cryptoStats:
   lastPrice = cryptoStat.find("span", class_="last-price")
   name = cryptoStat.find("div", class_="name")
   ticker = cryptoStat.find("div", class_="label ticker")
-  dictOfTickerToLastPrices[ticker.text.strip().lower()] = lastPrice.text.strip()
+  percent24hChange = cryptoStat.find("span", class_="change-percent")
+  dictOfTickerToLastPrices[ticker.text.strip().lower()] = [
+    lastPrice.text.strip(),percent24hChange.text.strip()
+  ]
   dictOfNameToTicker[name.text.strip().lower()] = ticker.text.strip().lower()
 
-
-while True:  
-  userInputNameOfCoin = input("Which coin would you like to see the statistics for?: ").lower()
+while True:
+  userInputNameOfCoin = input(
+    "Which coin would you like to see the statistics for?: ").lower()
   if userInputNameOfCoin not in dictOfNameToTicker and userInputNameOfCoin not in dictOfTickerToLastPrices:
     print("That is not a valid coin name")
-  elif userInputNameOfCoin not in dictOfTickerToLastPrices :
+  elif userInputNameOfCoin not in dictOfTickerToLastPrices:
     ticker = dictOfNameToTicker[userInputNameOfCoin]
+    print(dictOfTickerToLastPrices[ticker])
   elif userInputNameOfCoin in dictOfTickerToLastPrices:
     print(dictOfTickerToLastPrices[userInputNameOfCoin])
-    
