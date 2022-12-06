bb7bb593e91985c62fe2b9afa62499dfe70e074f
roberto <63375372+xROBERTINOx@users.noreply.github.com>
2022-12-06 03:42:31 +0000

    literally changed the whole thing, and it isn't working perfectly, working on getting the different price changes in percent from each coin from different time segments listed on website.

--- main.py
+++ main.py
@@ -12,24 +12,37 @@ cryptoStats = results.find_all("div", class_="content-wrap")
 dictOfTickerToLastPrices = {}
 dictOfNameToTicker = {}
 
-# add check for price change of other times (week)
+# zoom in some more on the newCryptoStats
 for cryptoStat in cryptoStats:
   lastPrice = cryptoStat.find("span", class_="last-price")
   name = cryptoStat.find("div", class_="name")
   ticker = cryptoStat.find("div", class_="label ticker")
+  print(ticker.text.strip())
+  newUrl = "https://public.com/crypto/" + ticker.text.strip()
+  newPage = requests.get(newUrl)
+  newSoup = BeautifulSoup(newPage.content, "html.parser")  
+  newResults = newSoup.find(id="__next")
+  newCryptoStats = newResults.find("div", class_="css-jhwu2z")
+  print(newCryptoStats)
+  newLastPrice = newCryptoStats.find("span", class_="css-12fcwpe")
+  print(newLastPrice);
+  # newCryptoStats = newResults.find_all("div", )
+# <span data-testid="asset-header-gain" class="css-1mhtxuu">$787.25 (4.79%)</span>
+  
   percent24hChange = cryptoStat.find("span", class_="change-percent")
   dictOfTickerToLastPrices[ticker.text.strip().lower()] = [
     lastPrice.text.strip(),percent24hChange.text.strip()
   ]
   dictOfNameToTicker[name.text.strip().lower()] = ticker.text.strip().lower()
 
-while True:
-  userInputNameOfCoin = input(
-    "Which coin would you like to see the statistics for?: ").lower()
-  if userInputNameOfCoin not in dictOfNameToTicker and userInputNameOfCoin not in dictOfTickerToLastPrices:
-    print("That is not a valid coin name")
-  elif userInputNameOfCoin not in dictOfTickerToLastPrices:
-    ticker = dictOfNameToTicker[userInputNameOfCoin]
-    print("The current price of " + userInputNameOfCoin.lower() + " is " + dictOfTickerToLastPrices[ticker][0] + ", and the % change in the last 24 hours is " + dictOfTickerToLastPrices[ticker][1])
-  elif userInputNameOfCoin in dictOfTickerToLastPrices:
-    print("The current price of " + userInputNameOfCoin.upper() + " is " + dictOfTickerToLastPrices[userInputNameOfCoin][0] + ", and the % change in the last 24 hours is " + dictOfTickerToLastPrices[userInputNameOfCoin][1])
+  
+# while True:
+  # userInputNameOfCoin = input(
+  #   "Which coin would you like to see the statistics for?: ").lower()
+  # if userInputNameOfCoin not in dictOfNameToTicker and userInputNameOfCoin not in dictOfTickerToLastPrices:
+  #   print("That is not a valid coin name")
+  # elif userInputNameOfCoin not in dictOfTickerToLastPrices:
+  #   ticker = dictOfNameToTicker[userInputNameOfCoin]
+  #   print("The current price of " + userInputNameOfCoin.lower() + " is " + dictOfTickerToLastPrices[ticker][0] + ", and the % change in the last 24 hours is " + dictOfTickerToLastPrices[ticker][1])
+  # elif userInputNameOfCoin in dictOfTickerToLastPrices:
+  #   print("The current price of " + userInputNameOfCoin.upper() + " is " + dictOfTickerToLastPrices[userInputNameOfCoin][0] + ", and the % change in the last 24 hours is " + dictOfTickerToLastPrices[userInputNameOfCoin][1])
